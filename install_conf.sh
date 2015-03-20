56330d21a3cb20593503494b98f0e981c74dfd37
Taris9047 <Taris9047@users.noreply.github.com>
2015-03-20 12:00:02 -0600

    Fixed installation script for dotfiles.

--- install_conf.sh
+++ install_conf.sh
@@ -5,22 +5,25 @@ echo "Target directory: ${USR_DIR}"
 echo "Source directory: ${CURRENT_DIR}"
 
 # Config Files
-CONF_LIST=(".vimrc" ".gvimrc" ".emacs")
+CONF_LIST=("vimrc" "gvimrc" "emacs")
+DOT="."
 echo ""
 for conf_file in ${CONF_LIST[*]}
 do
-    echo "Installing: ${conf_file}"
-    ln -sfv $CURRENT_DIR/$conf_file $USR_DIR/$conf_file
+    echo "Installing: ${DOT}${conf_file}"
+    rm -rvf $USR_DIR/$DOT$conf_file
+    ln -sfv $CURRENT_DIR/dotfiles/$conf_file $USR_DIR/$DOT$conf_file
 done
 
 # Config Directories
 # On Linux, this part is unnecessary. However, on OS X or Freebsd..
 # ln works differently for directories.
-CONF_LIST_D=(".emacs.d")
+CONF_LIST_D=("emacs.d")
 for conf_dir in ${CONF_LIST_D[*]}
 do
     echo "Installing: ${conf_dir}"
-    ln -sfv $CURRENT_DIR/$conf_dir $USR_DIR/
+    rm -rfv $USR_DIR/$DOT$conf_dir
+    ln -sfv $CURRENT_DIR/dotfiles/$conf_dir $USR_DIR/$DOT$conf_dir
 done
 
 echo ""
