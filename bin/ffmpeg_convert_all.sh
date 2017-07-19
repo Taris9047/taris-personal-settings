11c18e1e841c8d9ff3ea16d75508e70b23d058eb
Taris9047 <talezshin@gmail.com>
2017-07-18 23:22:39 -0500

    w--t

--- bin/ffmpeg_convert_all.sh
+++ bin/ffmpeg_convert_all.sh
@@ -19,20 +19,35 @@ echo "Setting up target format as $TARGET_FORMAT"
 
 echo "Let's work!!"
 
+MAIN_OPTS="-y"
+
 TARGET_ENC_PHRASE=""
 if [ "$TARGET_FORMAT" == "mp3" ]; then
-	TARGET_ENC_PHRASE="-acodec libmp3lame -b:a 192k -af dynaudnorm"
+	TARGET_ENC_PHRASE="$MAIN_OPTS -acodec libmp3lame -b:a 192k -af dynaudnorm"
 elif [ "$TARGET_FORMAT" == "m4a" ]; then
-	TARGET_ENC_PHRASE="-c:a libfdk_aac -b:a 192k -af dynaudnorm"
+	TARGET_ENC_PHRASE="$MAIN_OPTS -c:a libfdk_aac -b:a 192k -af dynaudnorm"
 elif [ "$TARGET_FORMAT" == "aac" ]; then
-    TARGET_ENC_PHRASE="-acodec aac -b:a 192k -af dynaudnorm"
+    TARGET_ENC_PHRASE="$MAIN_OPTS -acodec aac -b:a 192k -af dynaudnorm"
 elif [ "$TARGET_FORMAT" == "ogg" ]; then
-    TARGET_ENC_PHRASE="-acodec libvorbis -q:a 2 -af dynaudnorm"
+    TARGET_ENC_PHRASE="$MAIN_OPTS -acodec libvorbis -q:a 2 -af dynaudnorm"
+elif [ "$TARGET_FORMAT" == "norm" ]; then
+	TARGET_ENC_PHRASE="$MAIN_OPTS -af dynaudnorm"
 fi
 
-for f in ./*.*[^$TARGET_FORMAT]; 
+if [ $TARGET_FORMAT == "norm" ]; then
+    TARGETS=./*.*
+else
+    TARGETS=./*.*[^$TARGET_FORMAT]
+fi
+
+for f in $TARGETS;
 do
-	$FFMPEG -i "$f" $TARGET_ENC_PHRASE "${f%.*}.$TARGET_FORMAT"
+    if [ $TARGET_FORMAT == "norm" ]; then
+	    TARGET_OUTF="$f"
+    else
+	    TARGET_OUTF="${f%.*}.$TARGET_FORMAT"
+    fi
+	$FFMPEG -i "$f" $TARGET_ENC_PHRASE "$TARGET_OUTF"
 done
 
 
