e54caefb405f90ef07a26cfe51b9ee256d4250f5
Taris9047 <talezshin@gmail.com>
2017-07-18 23:37:38 -0500

    oops, normalization breaks file..
    fixed..

--- bin/ffmpeg_convert_all.sh
+++ bin/ffmpeg_convert_all.sh
@@ -43,11 +43,17 @@ fi
 for f in $TARGETS;
 do
     if [ $TARGET_FORMAT == "norm" ]; then
-	    TARGET_OUTF="$f"
+	    TARGET_OUTF=${f%.*}.norm.$TARGET_FORMAT"
     else
 	    TARGET_OUTF="${f%.*}.$TARGET_FORMAT"
     fi
+
 	$FFMPEG -i "$f" $TARGET_ENC_PHRASE "$TARGET_OUTF"
+
+    if [ $TARGET_FORMAT == "norm" ]; then
+        rm -rf "$f"
+        mv "$TARGET_OUTF" "$f"
+    fi
 done
 
 
