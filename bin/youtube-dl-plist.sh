405bc43e793e104db9b0f843571141f477a12415
Taylor Shin <talezshin@gmail.com>
2017-11-18 20:59:27 -0600

    Some changes...

--- bin/youtube-dl-plist.sh
+++ bin/youtube-dl-plist.sh
@@ -4,12 +4,14 @@ YOUTUBE_DL=$(command -v youtube-dl)
 
 if [ $YOUTUBE_DL == "" ]; then
     echo "Can't find youtube-dl!!"
-    return
+    exit -1
 fi
+echo "youtube-dl has been found at $YOUTUBE_DL"
+
 
 if [ $# -eq 0 ]; then
     echo "Need an address!!"
-    return
+    exit -1
 fi
 
 $YOUTUBE_DL -f bestaudio --audio-quality 0 --audio-format m4a -i -x --extract-audio -o '%(title)s.%(ext)s' $1
