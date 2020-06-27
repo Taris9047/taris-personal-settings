07039ed01403d3f4f43f81ff1c0c8411ba0cdb39
Taris' stuffs <talezshin@gmail.com>
2020-06-27 07:00:52 -0300

    so many kite stuffs

--- install_fira_fonts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-wget https://github.com/mozilla/Fira/archive/master.zip
-unzip master.zip
-sudo mkdir -p /usr/share/fonts/truetype/fira
-sudo cp Fira-master/ttf/* /usr/share/fonts/truetype/fira
