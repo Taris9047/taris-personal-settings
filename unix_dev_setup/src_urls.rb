3e48a87be52f3c7f19276aea0ebc6b5ba3878bf9
Taris' stuffs <talezshin@gmail.com>
2020-12-27 19:40:02 -0400

    Implemented some workaround to avoid hjson module...
    Not sure why, but they aren't really updating hjson gem on ruby for
    newer interpreters. So, just made some workaround to ignore comment
    lines in the urls.json.
    
    Any violation of JSON grammar will be caught up and break the program.

--- unix_dev_setup/src_urls.rb
+++ unix_dev_setup/src_urls.rb
@@ -2,19 +2,52 @@
 
 # URL database interfacing part...
 # --> JSON would be suffice, right?
+#
+# require 'hjson'
+#
+# class ParseHjson
+#   def initialize(json_path='./')
+#     json_fp = File.read(File.join(json_path, 'urls.json'))
+#     @URL_DB = Hjson.parse(json_fp)
+#   end
+#
+#   def Get(pkg_name)
+#     return @URL_DB[pkg_name]
+#   end
+# end # class parse_json
 
-require 'hjson'
+
+##
+## Due to deprecation of hjson on ruby platform(especially on newer ones),
+## we decided to parse the json text by ourselves.
+##
+
+$def_db_json_name = 'urls.json'
+
+require 'json'
 
 class ParseHjson
   def initialize(json_path='./')
-    json_fp = File.read(File.join(json_path, 'urls.json'))
-    @URL_DB = Hjson.parse(json_fp)
+    real_f_name = File.join(json_path, $def_db_json_name)
+    @json_file = File.read(real_f_name)
+    @json_data = File.readlines(real_f_name)
+    @cleaned_up_data = []
+    for line in @json_data
+      unless line.include?('#')
+        cleaned_up_line = line.delete('\n').delete(' ')
+        if cleaned_up_line.size > 1
+          @cleaned_up_data.push(cleaned_up_line)
+        end
+      end
+    end
+    @cleaned_up_data = @cleaned_up_data.join('')
+    @URL_DB = JSON.parse(@cleaned_up_data)
   end
 
   def Get(pkg_name)
     return @URL_DB[pkg_name]
   end
-end # class parse_json
+end # class ParseHjson
 
 module SRC_URL
 
@@ -25,5 +58,3 @@ module SRC_URL
 
   module_function :[]
 end # class URL_DB
-
-
