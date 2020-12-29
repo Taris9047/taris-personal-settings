f0e87879015b5bcdb9c4e22e7d3d9da58af2d771
Taris' stuffs <talezshin@gmail.com>
2020-12-29 18:24:30 -0400

    eDEX-UI run script added.

---
+++ bin/run_edexui.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+eDEXUI_DIR=$HOME/.opt/edex-ui
+
+if [ -d $eDEXUI_DIR ]; then
+	echo "It seems we don't have eDEX-UI on the system!"
+	echo "Visit:"
+	echo "https://github.com/GitSquared/edex-ui"
+	echo "To install it. It might need Node.JS to be installed prior to"
+	echo "be compiled."
+	exit(0)
+else
+	cd $eDEXUI_DIR && npm start
+fi
