57d483adebe6e30ff0c203976722cd6640c5eb40
talezshin <talezshin@gmail.com>
2021-01-12 19:36:39 -0400

    Always, last minute changes..

--- bin/setup_rust.sh
+++ bin/setup_rust.sh
@@ -3,6 +3,8 @@
 if [ ! -x $(command -v cargo) ]; then
     echo "Looks like we don't have Rust! Installing from main repo!"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+else
+    rustup update
 fi
 
 inst_cargo_pkgs ()
