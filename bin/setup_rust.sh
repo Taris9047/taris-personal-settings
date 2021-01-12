02cda57a379b5e1bc92f7e6f8ecf7302d01b6397
talezshin <talezshin@gmail.com>
2021-01-12 19:34:28 -0400

    Working on rust installation script

---
+++ bin/setup_rust.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ ! -x $(command -v cargo) ]; then
+    echo "Looks like we don't have Rust! Installing from main repo!"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+inst_cargo_pkgs ()
+{
+    pkgs="exa lsd bat cargo-update diskonaut rm-improved starship tokei"
+    cargo install $pkgs
+}
+
+inst_cargo_pkgs
+
