e3343fca027740c13b38b95fc3a5233523b4fa92
talezshin <talezshin@gmail.com>
2021-01-12 19:35:09 -0500

    ouch

--- bin/setup_rust.sh
+++ bin/setup_rust.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ ! -x $(command -v cargo) ]; then
-    echo "Looks like we don't have Rust! Installing from main repo!"
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-else
+if ! [ -x $(command -v cargo) ]; then
     rustup update
+else
+	echo "Looks like we don't have Rust! Installing from main repo!"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 inst_cargo_pkgs ()
