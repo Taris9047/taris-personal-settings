ab3aa96a8353844778bffca788e8bd7bc19b26c1
talezshin <talezshin@gmail.com>
2021-02-20 15:24:03 -0400

    Adopted colorscripts from DistroTube.
    And installed the so called 'fastest' terminal emulator: Alacritty.
    
    Not sure they would be used but. well..

---
+++ bin/install_colorscripts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://gitlab.com/dwt1/shell-color-scripts.git
+cd shell-color-scripts 
+rm -rf /opt/shell-color-scripts || return 1
+sudo mkdir -p /opt/shell-color-scripts/colorscripts || return 1
+sudo cp -rf colorscripts/* /opt/shell-color-scripts/colorscripts
+sudo cp colorscript.sh /usr/bin/colorscript
+rm -rf ./shell-color-scripts || return 1
+
+# optional for zsh completion
+if [ -x $(command -v zsh) ]; then
+  sudo cp zsh_completion/_colorscript /usr/share/zsh/site-functions
+fi
