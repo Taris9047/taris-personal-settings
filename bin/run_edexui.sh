bb30ff18403e2d1886b00f46f7a5f8585258822b
talezshin <talezshin@gmail.com>
2021-03-13 22:54:15 -0400

    "Updated silly edexui running script."

--- bin/run_edexui.sh
+++ bin/run_edexui.sh
@@ -2,15 +2,22 @@
 
 eDEXUI_DIR=$HOME/.opt/edex-ui
 
-if [ ! -d $eDEXUI_DIR ]; then
+if [ ! -x "$(command -v npm)" ]; then
+	echo "eDEXUI needs Node.JS and npm!!"
+	exit 1
+fi
+
+if [ ! -d "${eDEXUI_DIR}" ]; then
 	echo "It seems we don't have eDEX-UI on the system!"
 	echo "Let's install it!"
-	mkdir -p $HOME/.opt
+	if [ ! -d "${HOME}/.opt" ]; then
+		mkdir -p $HOME/.opt
+	fi
 	git clone https://github.com/GitSquared/edex-ui.git $eDEXUI_DIR
 	cd $eDEXUI_DIR && npm run install-linux
 	exit 0
 fi
 
-if [ -d $eDEXUI_DIR ]; then
+if [ -d "${eDEXUI_DIR}" ]; then
 	cd $eDEXUI_DIR && npm start
 fi
