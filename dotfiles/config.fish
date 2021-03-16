f3db4ec7ab262704c2d03a678b0775c62f43fab5
talezshin <talezshin@gmail.com>
2021-03-16 04:03:47 -0300

    "Still working on fish"

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -48,61 +48,6 @@ set fish_color_command brcyan
 set fish_color_error '#ff6c6b'
 set fish_color_param brcyan
 
-### SPARK ###
-set -g spark_version 1.0.0
-
-complete -xc spark -n __fish_use_subcommand -a --help -d "Show usage help"
-complete -xc spark -n __fish_use_subcommand -a --version -d "$spark_version"
-complete -xc spark -n __fish_use_subcommand -a --min -d "Minimum range value"
-complete -xc spark -n __fish_use_subcommand -a --max -d "Maximum range value"
-
-function spark -d "sparkline generator"
-    if isatty
-        switch "$argv"
-            case {,-}-v{ersion,}
-                echo "spark version $spark_version"
-            case {,-}-h{elp,}
-                echo "usage: spark [--min=<n> --max=<n>] <numbers...>  Draw sparklines"
-                echo "examples:"
-                echo "       spark 1 2 3 4"
-                echo "       seq 100 | sort -R | spark"
-                echo "       awk \\\$0=length spark.fish | spark"
-            case \*
-                echo $argv | spark $argv
-        end
-        return
-    end
-
-    command awk -v FS="[[:space:],]*" -v argv="$argv" '
-        BEGIN {
-            min = match(argv, /--min=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
-            max = match(argv, /--max=[0-9]+/) ? substr(argv, RSTART + 6, RLENGTH - 6) + 0 : ""
-        }
-        {
-            for (i = j = 1; i <= NF; i++) {
-                if ($i ~ /^--/) continue
-                if ($i !~ /^-?[0-9]/) data[count + j++] = ""
-                else {
-                    v = data[count + j++] = int($i)
-                    if (max == "" && min == "") max = min = v
-                    if (max < v) max = v
-                    if (min > v ) min = v
-                }
-            }
-            count += j - 1
-        }
-        END {
-            n = split(min == max && max ? "▅ ▅" : "▁ ▂ ▃ ▄ ▅ ▆ ▇ █", blocks, " ")
-            scale = (scale = int(256 * (max - min) / (n - 1))) ? scale : 1
-            for (i = 1; i <= count; i++)
-                out = out (data[i] == "" ? " " : blocks[idx = int(256 * (data[i] - min) / scale) + 1])
-            print out
-        }
-    '
-end
-### END OF SPARK ###
-
-
 ### Basic Aliases ###
 alias sudo="sudo -H"
 
@@ -111,3 +56,61 @@ if test -x 'nvim';
     echo "neovim not found int the path!"
 end
 alias vim='nvim'
+
+### Extraction ###
+function ex
+  if test -f $argv[1]
+    switch $argv[1]
+    case '*.tar.bz2'
+      tar xjf $argv[1]
+    case '*.tar.gz'
+      tar xzf $argv[1]
+    case '*.bz2'
+      bunzip2 $argv[1]
+    case '*.rar'
+      unrar x $argv[1]
+    case '*.gz'
+      gunzip $argv[1]
+    case '*.tar'
+      tar xf $argv[1]
+    case '*.tbz2'
+      tar xjf $argv[1]
+    case '*.tgz'
+      tar xzf $argv[1] 
+    case '*.zip'
+      unzip $argv[1]
+    case '*.Z'
+      uncompress $argv[1]
+    case '*.7z'
+      7z x $argv[1]
+    case '*.deb'
+      ar x $argv[1]
+    case '*.tar.xz'
+      tar xf $argv[1]
+    case '*.tar.zst'
+      unzstd $argv[1]
+    case '*'
+      echo "$argv[1] cannot be extracted via ex() yet."
+    end
+  else
+    echo "$argv[1] is not a valid file!"
+  end
+end
+
+### Navigate ###
+function up
+  set d ""
+  set limit "$argv[1]"
+
+  if test -z "$limit" -o "$limit" -le 0
+    set limit 1
+  end
+
+  for i in (seq 0 "$limit")
+    set d "../$d"
+  end
+
+  if ! cd "$d"
+    echo "Couldn't go up $limit dirs."
+  end
+end
