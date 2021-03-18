af6b326bc8799b4aa54627457d1b0ef5b88286fc
talezshin <talezshin@gmail.com>
2021-03-17 21:32:20 -0300

    Updated nanum font installation script. Now downloads the fonts from my own repo. They are free to distribute fonts.

--- bin/install_nanum_fonts.sh
+++ bin/install_nanum_fonts.sh
@@ -16,16 +16,18 @@ CWD=$(pwd -P)
 HOMEBREW="${HOME}/.local"
 TARGET_DIR="${HOMEBREW}/share/fonts/NanumFonts"
 
-NGF_URL="http://cdn.naver.com/naver/NanumFont/fontfiles/NanumFont_TTF_ALL.zip"
-NGFC_URL="https://github.com/naver/nanumfont/releases/download/VER2.5/NanumGothicCoding-2.5.zip"
+NF_URL="https://github.com/Taris9047/taris-personal-settings/releases/download/Nanum/NanumFonts.zip"
+# NGF_URL="http://cdn.naver.com/naver/NanumFont/fontfiles/NanumFont_TTF_ALL.zip"
+# NGFC_URL="https://github.com/naver/nanumfont/releases/download/VER2.5/NanumGothicCoding-2.5.zip"
 
 TEMP_DIR="${CWD}/.temp_nanumfonts"
 if [ ! -d "${TEMP_DIR}" ]; then
     mkdir -p "${TEMP_DIR}"
 fi
 
-wget "$NGF_URL" -O "$TEMP_DIR/ngf.zip"
-wget "$NGFC_URL" -O "$TEMP_DIR/ngfc.zip"
+# wget "$NGF_URL" -O "$TEMP_DIR/ngf.zip"
+# wget "$NGFC_URL" -O "$TEMP_DIR/ngfc.zip"
+wget "$NF_URL" -O "$TEMP_DIR/nf.zip"
 
 if [ ! -d "${TARGET_DIR}" ]; then
     mkdir -p "${TARGET_DIR}"
@@ -33,11 +35,11 @@ fi
 
 # unzip "${TEMP_DIR}/ngf.zip" -do "${TARGET_DIR}/"
 # unzip "${TEMP_DIR}/ngfc.zip" -do "${TARGET_DIR}/"
-cd "${TARGET_DIR}" && unzip "${TEMP_DIR}/ngf.zip"
-cd "${TARGET_DIR}" && unzip "${TEMP_DIR}/ngfc.zip"
+# cd "${TARGET_DIR}" && unzip "${TEMP_DIR}/ngf.zip"
+# cd "${TARGET_DIR}" && unzip "${TEMP_DIR}/ngfc.zip"
+cd "${TARGET_DIR}" && unzip "${TEMP_DIR}/nf.zip"
 
-
-rm -rf "${TARGET_DIR}/__MACOSX"
+# rm -rf "${TARGET_DIR}/__MACOSX"
 
 fc-cache -fv
 
