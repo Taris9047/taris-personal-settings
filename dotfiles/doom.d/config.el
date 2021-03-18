308919e7558d0db97c7aaf188ca54c4e6273a508
talezshin <talezshin@gmail.com>
2021-03-18 04:19:06 -0300

    "Tangling works!"

--- dotfiles/doom.d/config.el
+++ dotfiles/doom.d/config.el
@@ -1,22 +1,295 @@
- *doom:eshell-popup:main*  Thu Mar 18 03:27:18 2021
+(map! :leader
+      :desc "List bookmarks"
+      "b L" #'list-bookmarks
+      :leader
+      :desc "Save current bookmarks to bookmark file"
+      "b w" #'bookmark-save)
 
-~/.s/dotfiles/doom.d [master] λ cat
-usage: cat [OPTION] FILE...
-Concatenate FILE(s), or standard input, to standard output.
+(setq centaur-tabs-set-bar 'over
+      centaur-tabs-set-icons t
+      centaur-tabs-gray-out-icons 'buffer
+      centaur-tabs-height 24
+      centaur-tabs-set-modified-marker t
+      centaur-tabs-style "bar"
+      centaur-tabs-modified-marker "•")
+(map! :leader
+      :desc "Toggle tabs globally"
+      "t c" #'centaur-tabs-mode
+      :leader
+      :desc "Toggle tabs local display"
+      "t C" #'centaur-tabs-local-mode)
+(evil-define-key 'normal centaur-tabs-mode-map (kbd "g <right>") 'centaur-tabs-forward        ; default Doom binding is 'g t'
+                                               (kbd "g <left>")  'centaur-tabs-backward       ; default Doom binding is 'g T'
+                                               (kbd "g <down>")  'centaur-tabs-forward-group
+                                               (kbd "g <up>")    'centaur-tabs-backward-group)
 
-    -h, --help           show this usage screen
+(map! :leader
+      :desc "Dired"
+      "d d" #'dired
+      :leader
+      :desc "Dired jump to current"
+      "d j" #'dired-jump
+      (:after dired
+       (:map dired-mode-map
+        :leader
+        :desc "Peep-dired image previews"
+        "d p" #'peep-dired
+        :leader
+        :desc "Dired view file"
+        "d v" #'dired-view-file)))
+(evil-define-key 'normal dired-mode-map
+  (kbd "h") 'dired-up-directory
+  (kbd "l") 'dired-open-file) ; use dired-find-file instead if not using dired-open package
+(evil-define-key 'normal peep-dired-mode-map
+  (kbd "j") 'peep-dired-next-file
+  (kbd "k") 'peep-dired-prev-file)
+(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+(setq dired-open-extensions '(("gif" . "sxiv")
+                              ("jpg" . "sxiv")
+                              ("png" . "sxiv")
+                              ("mkv" . "mpv")
+                              ("mp4" . "mpv")))
 
-This command is implemented in Lisp.  If an unrecognized option is
-passed to this command, the external version ‘/bin/cat’
-will be called instead.
+(map! :leader
+      :desc "Winner redo"
+      "w <right>" #'winner-redo
+      :leader
+      :desc "Winner undo"
+      "w <left>" #'winner-undo)
 
-~/.s/dotfiles/doom.d [master] λ pwd
-~/.settings/dotfiles/doom.d
+(map! :map evil-window-map
+      "SPC" #'rotate-layout
+      ;; Navigation
+      "<left>"  #'evil-window-left
+      "<down>"  #'evil-window-down
+      "<up>"    #'evil-window-up
+      "<right>" #'evil-window-right
+      ;; Swapping Windows
+      "C-<left>"        #'+evil/window-move-left
+      "C-<down>"        #'+evil/window-move-down
+      "C-<up>"          #'+evil/window-move-up
+      "C-<right>"       #'+evil/window-move-right)
 
-~/.s/dotfiles/doom.d [master] λ org-babel-tanble
-org-babel-tanble: command not found
+(setq display-line-numbers-type t)
+(map! :leader
+      :desc "Comment or uncomment lines"
+      "TAB TAB" #'comment-line
+      :leader
+      :desc "Toggle line numbers"
+      "t l" #'doom/toggle-line-numbers
+      :leader
+      :desc "Toggle line highlight in frame"
+      "t h" #'hl-line-mode
+      :leader
+      :desc "Toggle line highlight globally"
+      "t H" #'global-hl-line-mode
+      :leader
+      :desc "Toggle truncate lines"
+      "t t" #'toggle-truncate-lines)
 
-~/.s/dotfiles/doom.d [master] λ org-babel-tangle
-Canceled
+(setq display-line-numbers-type t)
+(map! :leader
+      :desc "Comment or uncomment lines"
+      "TAB TAB" #'comment-line
+      :leader
+      :desc "Toggle line numbers"
+      "t l" #'doom/toggle-line-numbers
+      :leader
+      :desc "Toggle line highlight in frame"
+      "t h" #'hl-line-mode
+      :leader
+      :desc "Toggle line highlight globally"
+      "t H" #'global-hl-line-mode
+      :leader
+      :desc "Toggle truncate lines"
+      "t t" #'toggle-truncate-lines)
 
-~/.s/dotfiles/doom.d [master] λ org-babel-tangle
+(map! :leader
+      :desc "Edit agenda file"
+      "- a" #'(lambda () (interactive) (find-file "~/Org/agenda.org"))
+      :leader
+      :desc "Edit doom config.org"
+      "- c" #'(lambda () (interactive) (find-file "~/.doom.d/config.org"))
+      :leader
+      :desc "Edit eshell aliases"
+      "- e" #'(lambda () (interactive) (find-file "~/.doom.d/aliases"))
+      :leader
+      :desc "Edit doom init.el"
+      "- i" #'(lambda () (interactive) (find-file "~/.doom.d/init.el"))
+      :leader
+      :desc "Edit doom packages.el"
+      "- p" #'(lambda () (interactive) (find-file "~/.doom.d/packages.el")))
+
+(after! neotree
+  (setq neo-smart-open t
+        neo-window-fixed-size nil))
+(after! doom-themes
+  (setq doom-neotree-enable-variable-pitch t))
+(map! :leader
+      :desc "Toggle neotree file viewer"
+      "t n" #'neotree-toggle
+      :leader
+      :desc "Open directory in neotree"
+      "d n" #'neotree-dir)
+
+(after! org
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  (setq org-directory "~/Org/"
+        org-agenda-files '("~/Org/agenda.org")
+        org-default-notes-file (expand-file-name "notes.org" org-directory)
+        org-ellipsis " ▼ "
+        org-log-done 'time
+        org-journal-dir "~/Org/journal/"
+        org-journal-date-format "%B %d, %Y (%A) "
+        org-journal-file-format "%Y-%m-%d.org"
+        org-hide-emphasis-markers t
+        ;; ex. of org-link-abbrev-alist in action
+        ;; [[arch-wiki:Name_of_Page][Description]]
+        org-link-abbrev-alist    ; This overwrites the default Doom org-link-abbrev-list
+          '(("google" . "http://www.google.com/search?q=")
+            ("arch-wiki" . "https://wiki.archlinux.org/index.php/")
+            ("ddg" . "https://duckduckgo.com/?q=")
+            ("wiki" . "https://en.wikipedia.org/wiki/"))
+        org-todo-keywords        ; This overwrites the default Doom org-todo-keywords
+          '((sequence
+             "TODO(t)"           ; A task that is ready to be tackled
+             "BLOG(b)"           ; Blog writing assignments
+             "GYM(g)"            ; Things to accomplish at the gym
+             "PROJ(p)"           ; A project that contains other tasks
+             "VIDEO(v)"          ; Video assignments
+             "WAIT(w)"           ; Something is holding up this task
+             "|"                 ; The pipe necessary to separate "active" states and "inactive" states
+             "DONE(d)"           ; Task has been completed
+             "CANCELLED(c)" )))) ; Task has been cancelled
+
+(setq user-full-name "Taylor Shin"
+      user-mail-address "talezshin@gmail.com")
+
+(setq doom-font (font-spec :family "mononoki Nerd Font Mono" :size 16)
+      doom-big-font (font-spec :family "mononoki Nerd Font Mono" :size 26)
+      doom-variable-pitch-font (font-spec :family "NanumGothic" :size 16)
+      doom-serif-font (font-spec :family "NanumGothic" :weight 'light))
+
+(if (string= (guess-linux-distribution) "elementary")
+    (setq doom-theme 'doom-solarized-light)
+  (setq doom-theme 'doom-palenight))
+
+(map! :leader
+      :desc "Load new theme"
+      "h t" #'counsel-load-theme)
+
+(defun dt/org-babel-tangle-async (file)
+  "Invoke `org-babel-tangle-file' asynchronously."
+  (message "Tangling %s..." (buffer-file-name))
+  (async-start
+   (let ((args (list file)))
+     `(lambda ()
+        (require 'org)
+        ;;(load "~/.emacs.d/init.el")
+        (let ((start-time (current-time)))
+          (apply #'org-babel-tangle-file ',args)
+          (format "%.2f" (float-time (time-since start-time))))))
+   (let ((message-string (format "Tangling %S completed after " file)))
+     `(lambda (tangle-time)
+        (message (concat ,message-string
+                         (format "%s seconds" tangle-time)))))))
+
+(defun dt/org-babel-tangle-current-buffer-async ()
+  "Tangle current buffer asynchronously."
+  (dt/org-babel-tangle-async (buffer-file-name)))
+
+(setq default-input-method "korean-hangul")
+(global-set-key (kbd "S-SPC") 'toggle-input-method)
+;; Be sure to enable hardware Hangul key from Keyboard(XKB) Options to use this.
+(global-set-key (kbd "<Hangul>") 'toggle-input-method)
+;; (global-set-key (kbd "<Ctrl_R>") 'toggle-input-method)
+(global-set-key (kbd "<Alt_R>") 'toggle-input-method)
+(when (eq system-type 'gnu/linux)
+  (set-fontset-font t 'hangul (font-spec :family "NotoSans CJK KR" :size 16))
+  )
+
+(defun prefer-horizontal-split ()
+  (set-variable 'split-height-threshold nil t)
+  (set-variable 'split-width-threshold 40 t)) ; make this as low as needed
+(add-hook 'markdown-mode-hook 'prefer-horizontal-split)
+(map! :leader
+      :desc "Clone indirect buffer other window"
+      "b c" #'clone-indirect-buffer-other-window)
+
+(map! :leader
+      :desc "Winner redo"
+      "w <right>" #'winner-redo
+      :leader
+      :desc "Winner undo"
+      "w <left>" #'winner-undo)
+
+(custom-set-variables
+ '(elfeed-feeds
+   (quote
+    (("https://www.reddit.com/r/linux.rss" reddit linux)
+     ("https://www.gamingonlinux.com/article_rss.php" gaming linux)
+     ("https://hackaday.com/blog/feed/" hackaday linux)
+     ("https://opensource.com/feed" opensource linux)
+     ("https://linux.softpedia.com/backend.xml" softpedia linux)
+     ("https://itsfoss.com/feed/" itsfoss linux)
+     ("https://www.zdnet.com/topic/linux/rss.xml" zdnet linux)
+     ("https://www.phoronix.com/rss.php" phoronix linux)
+     ("http://feeds.feedburner.com/d0od" omgubuntu linux)
+     ("https://www.computerworld.com/index.rss" computerworld linux)
+     ("https://www.networkworld.com/category/linux/index.rss" networkworld linux)
+     ("https://www.techrepublic.com/rssfeeds/topic/open-source/" techrepublic linux)
+     ("https://betanews.com/feed" betanews linux)
+     ("http://lxer.com/module/newswire/headlines.rss" lxer linux)
+     ("https://distrowatch.com/news/dwd.xml" distrowatch linux)))))
+
+(emms-all)
+(emms-default-players)
+(emms-mode-line 1)
+(emms-playing-time 1)
+(setq emms-source-file-default-directory "~/Music/Non-Classical/70s-80s/"
+      emms-playlist-buffer-name "*Music*"
+      emms-info-asynchronously t
+      emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find)
+(map! :leader
+      :desc "Go to emms playlist"
+      "a a" #'emms-playlist-mode-go
+      :leader
+      :desc "Emms pause track"
+      "a x" #'emms-pause
+      :leader
+      :desc "Emms stop track"
+      "a s" #'emms-stop
+      :leader
+      :desc "Emms play previous track"
+      "a p" #'emms-previous
+      :leader
+      :desc "Emms play next track"
+      "a n" #'emms-next)
+
+(map! :leader
+      :desc "Evaluate elisp in buffer"
+      "e b" #'eval-buffer
+      :leader
+      :desc "Evaluate defun"
+      "e d" #'eval-defun
+      :leader
+      :desc "Evaluate elisp expression"
+      "e e" #'eval-expression
+      :leader
+      :desc "Evaluate last sexpression"
+      "e l" #'eval-last-sexp
+      :leader
+      :desc "Evaluate elisp in region"
+      "e r" #'eval-region)
+
+(setq browse-url-browser-function 'eww-browse-url)
+(map! :leader
+      :desc "Eww web browser"
+      "e w" #'eww
+      :leader
+      :desc "Eww reload page"
+      "e R" #'eww-reload
+      :leader
+      :desc "Search web for text between BEG/END"
+      "s w" #'eww-search-words)
