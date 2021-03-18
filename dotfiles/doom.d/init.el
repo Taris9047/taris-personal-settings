b02df3db263ad517d1aa793000437e4c190e2f74
talezshin <talezshin@gmail.com>
2021-03-18 01:47:43 -0300

    "Still workong on config.org"

--- dotfiles/doom.d/init.el
+++ dotfiles/doom.d/init.el
@@ -41,21 +41,20 @@
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
        (ligatures +extra)         ; ligatures and symbols to make your code pretty again
-       minimap           ; show a map of the code on the side
+       ;; minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
        neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup
-        +all
         +defaults)   ; tame sudden yet inevitable temporary windows
-       ;;tabs              ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       ;; tabs              ; a tab bar for Emacs
+       ;; treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       (window-select
-        +numbers); visually switch windows
+       ;; (window-select
+        ;; +numbers); visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        zen               ; distraction-free coding or writing
 
@@ -168,7 +167,9 @@
         +pandoc
         +gnuplot
         ;;+pomodoro
-        +present)
+        +present
+        +org-bullets
+        +org-ql)
        php               ; perl's insecure younger brother
        ;;perl
        ;;plantuml          ; diagrams for confusing people more
@@ -198,10 +199,10 @@
 
        :app
        ;;calendar
-       emms
-       everywhere        ; *leave* Emacs!? You must be joking
-       irc               ; how neckbeards socialize
-       (rss +org)        ; emacs as an RSS reader
+       ;; emms
+       ;; everywhere        ; *leave* Emacs!? You must be joking
+       ;; irc               ; how neckbeards socialize
+       ;; (rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :emms
