9d485d45b2ca875d0c7c24d50aacceb37ec266ed
talezshin <talezshin@gmail.com>
2021-03-19 12:14:49 -0300

    "Revised ls selection."

--- unix_dev_setup/setup_rust.sh
+++ unix_dev_setup/setup_rust.sh
@@ -20,8 +20,8 @@ rust_pkgs=( \
   "ytop" \
   "grex" \
   "zoxide" \
-  "nu" \
-  "broot" )
+  "broot" \
+)
 
 array_to_string ()
 {
@@ -29,20 +29,31 @@ array_to_string ()
   echo ${arr[*]}
 }
 
-if [ -x "$(command -v cargo)" ]; then
-  rustup update
-  cargo install-update -a
-else
-  echo "Looks like we don't have Rust! Installing from main repo!"
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-  source $HOME/.bashrc
-fi
-
 inst_cargo_pkgs ()
 {
   $HOME/.cargo/bin/cargo install $( array_to_string "${rust_pkgs[@]}" )
   # $HOME/.cargo/bin/cargo install-update -a
 }
+
+
+REINSTALL_PKGS=false
+if [ ! -z $@ ]; then
+  if [[ "$1" == "-f" || "$1" == "--force" ]]; then
+    REINSTALL_PKGS=true
+  fi
+fi
+
+if [ -x "$(command -v cargo)" ]; then
+  rustup update
+  cargo install-update -a
+  if [ $REINSTALL_PKGS ]; then
+    inst_cargo_pkgs
+  fi
+else
+  echo "Looks like we don't have Rust! Installing from main repo!"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  source $HOME/.bashrc
+fi
 
 if [ -d $HOME/.cargo ]; then
   inst_cargo_pkgs
