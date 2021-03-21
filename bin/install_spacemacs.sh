90373e8f8e10dee8b321bedc47a6ed5dab5fdf1d
talezshin <talezshin@gmail.com>
2021-03-21 07:57:06 -0300

    Ok, let's set up spacemacs later.

--- bin/install_spacemacs.sh
+++ bin/install_spacemacs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+
 echo "Installing Spacemacs!!"
 
 echo "Removing previous emacs!"
@@ -7,5 +9,6 @@ rm -rf $HOME/.emacs $HOME/.emacs.d $HOME/.doom.d
 
 echo "To the infinity!! and beyond!!"
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+ln -sfv "$SCRIPTPATH/../dotfiles/spacemacs" "$HOME/.spacemacs"
 
 echo "Spacemacs downloaded! Run emacs to make it working!"
