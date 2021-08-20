2e10dded00dcfdb7cefd736b49239b72e7b3e009
Taylor Shin <talezshin@gmail.com>
2021-08-20 09:05:53 -0400

    Setting up vpn connection.

---
+++ bin/vpn_connect_ucsc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VPN_SERVER="vpn.ucsc.edu"
+
+echo "Connectiong to UCSC Campus VPN"
+
+# VPN PATH
+VPN="/opt/cisco/anyconnect/bin/vpn"
+
+"$VPN" -s < $HOME/.vpn_creds connect "${VPN_SERVER}"
+
