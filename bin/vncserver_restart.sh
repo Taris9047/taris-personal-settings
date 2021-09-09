9834e1a6b106783e0bd5452650ecd20bac47b5b2
Taylor Shin <talezshin@gmail.com>
2021-09-09 18:15:30 -0400

    "Updated vpn/vnc stuffs."

--- bin/vncserver_restart.sh
+++ bin/vncserver_restart.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# This script is supposed to be run as a root user.
+printf  'RealVNC maintenance script.\n'
 
-sudo systemctl stop vncserver-x11-serviced.service
+if [ ! -x "$(command -v vncserver-x11)" ]; then
+  printf 'Looks like we are missing some components!\n'
+  exit 1
+fi
 
-sleep 1.0
+sudo systemctl stop vncserver-x11-serviced.service && sleep 1.0
 
-sudo systemctl start vncserver-x11-serviced.service
+sudo systemctl start vncserver-x11-serviced.service && printf 'RealVNC server successfully restarted!\n'
 
-printf 'RealVNC server restart finished!\n'
+sudo /usr/bin/vncserver-x11 -service -reload && printf 'RealVNC server reloaded!\n'
+
--- bin/vpn_connect_ucsc.sh
+++ bin/vpn_connect_ucsc.sh
@@ -2,6 +2,11 @@
 
 VPN_SERVER="vpn.ucsc.edu"
 
+if [ ! -f "$HOME/.vpn_creds" ]; then
+  printf 'We are lacking .vpn_creds in the user's home dir.\n'
+  exit 1
+fi
+
 echo "Connectiong to UCSC Campus VPN"
 
 # VPN PATH
@@ -9,5 +14,5 @@ VPN="/opt/cisco/anyconnect/bin/vpn"
 
 "$VPN" disconnect
 sleep 1
-"$VPN" -s < $HOME/.vpn_creds connect "${VPN_SERVER}"
+"$VPN" -s < "$HOME/.vpn_creds" connect "${VPN_SERVER}"
 
