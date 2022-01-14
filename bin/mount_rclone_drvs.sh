83b02bb879f87a8c591e91832702552cc250e3e1
Taylor Shin <talezshin@gmail.com>
2022-01-14 14:30:54 -0500

    "Still fixing the rclone code."

--- bin/mount_rclone_drvs.sh
+++ bin/mount_rclone_drvs.sh
@@ -8,13 +8,34 @@ fi
 RClone="$(command -v rclone)"
 
 # Mounting Google Drive
-${RClone} mount google-drive: /home/taris/.google-drive
+if [ -x "$(command -v rclone)" ]; then
+
+	if grep -qs "$GOOGLE_DRIVE" '/proc/mounts'; then
+		sleep 1.25
+	elif [ ! -f "$HOME/.config/rclone/rclone.conf" ]; then
+	  sleep 1.25
+  else
+		if [ ! -z "$(grep -i "\[google-drive\]" "$HOME/.config/rclone/rclone.conf")" ]; then
+			${RClone} mount google-drive: "$GOOGLE_DRIVE" &
+			sleep 2
+		fi
+	fi
+fi
 
 sleep 1.2
 
 # Mounting MS Onedrive
 ${RClone} mount --vfs-cache-mode writes onedrive: /home/taris/.onedrive
 
-sleep 1.2
+if grep -qs "$ONE_DRIVE" '/proc/mounts'; then
+    sleep "${line_delay}"
+elif [ ! -f "$HOME/.config/rclone/rclone.conf" ]; then
+    sleep 1.25
+else
+    if [ ! -z "$(grep -i "\[onedrive\]" "$HOME/.config/rclone/rclone.conf")" ]; then
+        ${RClone} mount --vfs-cache-mode writes onedrive: "$ONE_DRIVE" &
+        sleep 2
+    fi
+fi
 
 printf 'Cloud drives are all mounted!!\n'
