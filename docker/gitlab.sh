e8e999bd26c97f0d8de9436de1c6586212e35f7d
Taylor Shin <talezshin@gmail.com>
2022-01-26 10:43:21 -0500

    Updated docker starting script for gitlab...

--- docker/gitlab.sh
+++ docker/gitlab.sh
@@ -6,19 +6,28 @@ if [ ! -d "$GITLAB_HOME" ]; then
 	sudo mkdir -p "$GITLAB_HOME"
 fi
 
-MAIN_PORT=30000
+MAIN_PORT="30000"
 
-HOSTNAME='taris9047.ddns.net'
+if [ "$MAIN_PORT" = '80' ]; then
+  EXT_URL="taris9047.ddns.net"
+else
+  EXT_URL="taris9047.ddns.net:$MAIN_PORT"
+fi
+
+printf "Hostname set: $HOSTNAME\n\n"
+printf "Running docker!!\n"
 docker run --detach \
-  --hostname=$HOSTNAME \
-  --env GITLAB_OMNIBUS_CONFIG="gitlab_rails['lfs_enabled'] = true;" \
-  --publish 9443:443 --publish $MAIN_PORT:80 --publish 9022:22 \
+  --hostname="$EXT_URL" \
+  --env GITLAB_OMNIBUS_CONFIG="external_url 'http://$EXT_URL/';gitlab_rails['lfs_enabled'] = true;" \
+  --publish $MAIN_PORT:$MAIN_PORT --publish 9022:22 \
   --name gitlab \
   --restart always \
   --volume $GITLAB_HOME/config:/etc/gitlab \
   --volume $GITLAB_HOME/logs:/var/log/gitlab \
   --volume $GITLAB_HOME/data:/var/opt/gitlab \
   --shm-size 256m \
+  --memory=2048m \
+  --cpus=2 \
   gitlab/gitlab-ce:latest
 
 #  --env GITLAB_OMNIBUS_CONFIG="external_url 'http://$HOSTNAME:30000'; gitlab_rails['lfs_enabled'] = true;" \
