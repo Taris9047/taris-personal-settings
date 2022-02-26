1c4b78ec125369b5b1997a252720fa82e2b7cbd7
Taylor Shin <talezshin@gmail.com>
2022-02-26 11:00:27 -0500

    "Supports batch install"

--- bin/pyenv_install.sh
+++ bin/pyenv_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! -x "$(command -v pyenv)" ]; then
     printf 'pyenv not found in the path!!\n'
@@ -11,13 +11,30 @@ PYCONF_OPTS='--enable-shared --enable-ipv6 --enable-unicode=ucs4 --with-threads 
 
 PY_CFLAGS='-O2 -march=native -fomit-frame-pointer'
 
-if [ -z "$1" ]; then
-    printf 'Usage: pyenv_install.sh <python_version>\n'
+usage () {
+    printf 'Python bath installation script using pyenv\n'
+    printf '\n'
+    printf 'Usage: pyenv_install.sh <python_version1> <python_version2> ...\n'
+}
+
+if [ $# -eq 0 ]; then
+    usage
     exit 0
 fi
 
 
 # run install
-PYTHON_CONFIGURE_OPTS="\"$PYCONF_OPTS\"" CFLAGS="\"$PY_CFLAGS\"" \
-  $PYENV_CMD install -f "$1"
 
+for arg in "$@"
+do
+    case "$arg" in
+        -h | --help)
+            usage
+            exit 0
+            ;;
+        *)
+            PYTHON_CONFIGURE_OPTS="\"$PYCONF_OPTS\"" CFLAGS="\"$PY_CFLAGS\"" \
+              $PYENV_CMD install -f "$arg"
+            ;;
+    esac
+done
