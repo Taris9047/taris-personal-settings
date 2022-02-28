f3b453b96f71c2950c6650139818c910f9941e68
Taylor Shin <talezshin@gmail.com>
2022-02-28 09:44:25 -0500

    Fixed infinite loop problem

--- bin/vpn_connect_ucsc.sh
+++ bin/vpn_connect_ucsc.sh
@@ -3,8 +3,8 @@
 VPN_SERVER="vpn.ucsc.edu"
 
 if [ ! -f "$HOME/.vpn_creds" ]; then
-  printf 'We are lacking .vpn_creds in the users home dir.\n'
-  exit 1
+    printf 'We are lacking .vpn_creds in the users home dir.\n'
+    exit 1
 fi
 
 # VPN PATH
@@ -14,67 +14,67 @@ echo "Connectiong to UCSC Campus VPN"
 
 # Prints simple usage function
 usage() {
-  printf 'usage: vpn_connect_ucsc.sh <options> <program>\n'
-  printf 'options:\n'
-  printf '-h or --help: Prints out this message.\n'
-  printf '-f or --force: Disconnects vpn then try to reconnect\n'
+    printf 'usage: vpn_connect_ucsc.sh <options> <program>\n'
+    printf 'options:\n'
+    printf '-h or --help: Prints out this message.\n'
+    printf '-f or --force: Disconnects vpn then try to reconnect\n'
 }
 
 # Parsing arguments
 prog=''
 while (($# > 0)); do
-  case "$1" in
-    -h | --help)
-      usage
-      exit 0
-      ;;
-    -f | --force)
-      printf 'Forcefully disconnecting the VPN to re-establish the connection\n'
-      "$VPN" disconnect
-      ;;
-    *)
-      prog="$1"
-      ;;
-  esac
+    case "$1" in
+        -h | --help)
+            usage
+            exit 0
+            ;;
+        -f | --force)
+            printf 'Forcefully disconnecting the VPN to re-establish the connection\n'
+            "$VPN" disconnect
+            shift
+            ;;
+        *)
+            prog="$1"
+            shift
+            ;;
+    esac
 done
 
-
-
 if [ -n "$1" ] && [ "$1" = "-f" ] || [ "$1" = "--force" ]; then
-  printf 'Forcefully disconnecting the VPN to re-establish the connection\n'
-  "$VPN" disconnect
+    printf 'Forcefully disconnecting the VPN to re-establish the connection\n'
+    "$VPN" disconnect
 fi
 
 prog=''
 if [ ! -z "$1" ]; then
-  prog="$1"
+    prog="$1"
 fi
 
-connect_ucsc_vpn () {
+connect_ucsc_vpn() {
 
-  # sleep 1
-  
-  if [ ! -z "$prog" ]; then
-    prog_ps="$(ps -A | grep "$prog")"
-    if [ -z "$prog_ps" ]; then
-      printf 'Not running the progress "%s" in background.\n' "$prog"
-      printf 'Thus, no reason to connect to VPN again!\n'
-      exit 0
+    # sleep 1
+
+    if [ ! -z "$prog" ]; then
+        prog_ps="$(ps -A | grep "$prog")"
+        if [ -z "$prog_ps" ]; then
+            printf 'Not running the progress "%s" in background.\n' "$prog"
+            printf 'Thus, no reason to connect to VPN again!\n'
+            exit 0
+        fi
     fi
-  fi
-    
-  if [ -n "$("${VPN}" state | grep -i 'Disconnected')" ]; then
-    "$VPN" -s < "$HOME/.vpn_creds" connect "${VPN_SERVER}" || connect_ucsc_vpn
-  else
-    printf 'It seems we are still on the VPN!\n'
-    exit 0
-  fi
-}; connect_ucsc_vpn
+
+    if [ -n "$("${VPN}" state | grep -i 'Disconnected')" ]; then
+        "$VPN" -s connect "${VPN_SERVER}" <"$HOME/.vpn_creds" || connect_ucsc_vpn
+    else
+        printf 'It seems we are still on the VPN!\n'
+        exit 0
+    fi
+}
+connect_ucsc_vpn
 
 # Restart the VNC server...
 SCRIPT_PATH="$HOME/.settings/bin/vncserver_restart.sh"
 if [ "$(id -u)" -eq 0 ]; then
-  printf 'Restarting RealVNC Server\n'
-  sudo $SCRIPT_PATH
+    printf 'Restarting RealVNC Server\n'
+    sudo $SCRIPT_PATH
 fi
-
