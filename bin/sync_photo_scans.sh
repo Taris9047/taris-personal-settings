6d55fbfcf2e5777b5d50074315c30fce8bdbca02
Taylor Shin <talezshin@gmail.com>
2022-09-11 15:49:53 -0300

    Lets sync the scanned photos..

---
+++ bin/sync_photo_scans.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SRC_DIR="/GamenMedia/Scan/"
+DEST_DIR="$HOME/GoogleDrive/Mom\ Stuff/아기앨범/"
+
+CMD="rsync -hvrPt $SRC_DIR $DEST_DIR"
+
+if [ ! -x "$(command -v rsync)" ]; then
+	cp -rfv "$SRC_DIR" "$DEST_DIR"
+fi
+
