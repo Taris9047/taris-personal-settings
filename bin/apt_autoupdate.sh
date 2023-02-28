0adc65265347a0c14ace396390362712845201a6
Taylor Shin <talezshin@gmail.com>
2023-02-27 19:21:21 -0500

    "APT autoupdate script added for UFSDCadsim"

---
+++ bin/apt_autoupdate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update the package lists
+apt update && apt upgrade -y
+
+# Check if a reboot is needed
+if [ -f /var/run/reboot-required ]; then
+  wall -n "System will be reboot after 5 minutes for update."
+  sleep 300
+  /usr/sbin/reboot
+else
+  wall -n "System packages updated."
+fi
