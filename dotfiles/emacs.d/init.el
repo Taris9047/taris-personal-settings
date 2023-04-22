2ce152424d0f1ff2731be9e0b1e23cf16c756c1b
Taylor Shin <talezshin@gmail.com>
2023-04-22 10:31:09 -0400

    Chagned vanilla emacs default fonts to...
    Monospace...

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -16,6 +16,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
    '(writeroom-mode which-key iedit graphviz-dot-mode doom-themes vterm eshell-syntax-highlighting projectile perspective toc-org use-package sudo-edit smex ruby-electric peep-dired org-bullets org-auto-tangle ob-ipython neotree markdown-mode magit-todos lua-mode json-mode js-comint ivy-rich ivy-posframe haskell-mode general flycheck fish-mode evil-collection emojify elpy doom-modeline dired-open dashboard counsel all-the-icons-dired)))
 (custom-set-faces
