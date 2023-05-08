017e79d100be344a013c01a6eca640b873ba0680
Taylor Shin <talezshin@gmail.com>
2023-05-08 10:53:00 -0400

    Less intrusive env variables...

--- dotfiles/Xilinx.sh
+++ dotfiles/Xilinx.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-XILINX="/opt/Xilinx/"
+XILINX_DIR="/opt/Xilinx/"
 XILINX_VERSION="2022.2"
 
-export PATH=$PATH:$XILINX/Vivado/$XILINX_VERSION/bin
-export PATH=$PATH:$XILINX/Vitis/$XILINX_VERSION/bin
-#export PATH=$PATH:/opt/Xilinx/Vitis_HLS/$XILINX_VERSION/bin
+if [ -d "$XILINX_DIR" ]; then
+    export XILINX="$XILINX_DIR"
+    export PATH=$PATH:$XILINX/Vivado/$XILINX_VERSION/bin
+    export PATH=$PATH:$XILINX/Vitis/$XILINX_VERSION/bin
+    # export PATH=$PATH:/opt/Xilinx/Vitis_HLS/$XILINX_VERSION/bin
+fi
 
