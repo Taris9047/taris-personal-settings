afc41e18703515a38dcba2c79bef8a8042e1e679
Taylor Shin <talezshin@gmail.com>
2024-08-15 21:55:56 -0700

    working on vnc for tightvnc server.

--- bin/vnc_on_shared_server.sh
+++ bin/vnc_on_shared_server.sh
@@ -1,32 +1,36 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 unalias vncstart 2> /dev/null
 unalias vnckill 2> /dev/null
 
+if [ -x "$(command -v vncstart)" ]; then
+  printf 'Xtightvnc is not found!\n'
+  exit 1
+fi
 
 # ports to ignore: 5901-10
 
 
 # Tightvnc case
 
-# Preferred port: 5903
-vncport=3
+# Preferred port: 5907
+vncport=7
 
 vnc_port_select_Xtightvnc() {
     all_vnc_servers="$(ps -ef | grep 'Xtightvnc' | awk 'match($0, / :[0-9]+/) {print substr( $0, RSTART, RLENGTH)}')"
     my_vnc_server="$(ps -ef | grep 'Xtightvnc' | awk 'match($0, / :[0-9]+/) {print substr( $0, RSTART, RLENGTH)}')"
-  
 
 }
-
-
-
 
 vncstart() {
-    vncserver :$vncport -geometry 1600x900 -depth 24
+    vncserver :${vncport} -geometry 1760x990 -depth 24
 }
+
 vnckill() {
     vncserver -kill :$vncport
 }
-printf 'vncserver is set to open at 59%02d\n' $vncport
+
+vncstart()
+
+printf 'vncserver is set to open at 59%02d\n' "${vncport}"
 
