2096cbb23a2b5bd18a730b72fbf728393fb9c6a6
Taylor Shin <talezshin@gmail.com>
2024-11-03 20:14:51 -0500

    VTE..

--- dotfiles/vte.sh
+++ dotfiles/vte.sh
@@ -1,3 +1,10 @@
 if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
-  source /etc/profile.d/vte.sh
+	if [ -f '/etc/profile.d/vte.sh' ]; then
+		source /etc/profile.d/vte.sh
+	fi
+
+	if [ -f '/etc/profile.d/vte-2.96.sh' ]; then
+		source /etc/profile.d/vte.sh
+	fi
+
 fi
