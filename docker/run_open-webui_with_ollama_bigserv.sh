d5fd19ba541701599414d21eee4c1729f3838301
Taylor Shin <talezshin@gmail.com>
2025-01-21 08:35:59 -0800

    Ollama-Webui stuff added as well

---
+++ docker/run_open-webui_with_ollama_bigserv.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#-e OLLAMA_API_BASE_URL=http://127.0.0.1:11434/api \
+
+docker run -d \
+  -p 3000:8080 \
+  --add-host=host.docker.internal:host-gateway \
+  -v open-webui:/app/backend/data \
+  --network=host \
+  --name open-webui \
+  --restart always ghcr.io/open-webui/open-webui:main
+
