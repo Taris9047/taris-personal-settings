7247d5bd9d2ae413f079f11dd91cf049ba0931e8
Taylor Shin <talezshin@gmail.com>
2025-11-28 17:46:43 -0500

    btop install added...

---
+++ bin/install_btop.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#
+# A simple scipt to install BTop
+#
+
+URL='https://github.com/aristocratos/btop.git'
+
+OLD_DIR=`pwd`
+WORK_DIR="$(mktemp -d -t btop-XXXXXXXX)"
+
+cd "${WORK_DIR}"
+
+git clone "${URL}" "${WORK_DIR}/btop"
+cd "#{WORK_DIR}/btop"
+
+make && sudo make install
+
+
+cd "$(OLD_DIR)"
