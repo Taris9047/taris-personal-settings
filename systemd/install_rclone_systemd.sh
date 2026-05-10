4e65b600d8314a87ffefd3e93fa8109d7eae7000
Taylor Shin <talezshin@gmail.com>
2026-05-10 07:36:24 -0400

    Enabled icloud rclone

--- systemd/install_rclone_systemd.sh
+++ systemd/install_rclone_systemd.sh
@@ -5,6 +5,8 @@
 
 cwd="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+rclone_conf="${HOME}/.config/rclone/rclone.conf"
+
 if [ ! -x "$(command -v rclone)" ]; then
   printf 'rclone is not installed in the system!! Quitting!!\n'
   exit 1
@@ -23,18 +25,36 @@ mkdir -p "${user_systemd_dir}"
 ln -sfv "${cwd}/rclone@.service" "${user_systemd_dir}/rclone@.service"
 find "${cwd}" -iname '*.env' -exec ln -sfv '{}' "${user_systemd_dir}" \;
 
+function enable_rclone_service () {
+  local service_name="$1"
+  if [[ -n "$2" ]]; then
+    local service_name_user_friendly="$2"
+  else
+    local service_name_user_friendly="${service_name}"
+  fi
+
+  local service_fname="rclone@${service_name}.service"
+
+  if [ ! -z "$(grep -i "\[${service_name}\]" "${rclone_conf}")" ]; then
+    if [ ! -L "${user_systemd_dir}/rclone@${service_name}.service" ]; then
+      printf 'Enabling %s\n' "${service_name_user_friendly}"
+      systemctl --user enable --now rclone@${service_name}.service
+    else
+      printf '%s is already enabled!!\n' "${service_name_user_friendly}"
+    fi
+  fi
+}
+
+
 # Currently, we have google-drive and onedrive
 #
 # Google Drive
-if [ ! -z "$(grep -i "\[google-drive\]" "${HOME}/.config/rclone/rclone.conf")" ]; then
-  printf 'Enabling GoogleDrive\n'
-  systemctl --user enable --now rclone@google-drive.service
-fi
+enable_rclone_service 'google-drive' 'GoogleDrive'
 # OneDrive
-if [ ! -z "$(grep -i "\[onedrive\]" "${HOME}/.config/rclone/rclone.conf")" ]; then
-  printf 'Enabling OneDrive\n'
-  systemctl --user enable --now rclone@onedrive.service
-fi
+enable_rclone_service 'onedrive' 'MS Onedrive'
+# Added iCloudDrive recently
+# iCloud Drive
+enable_rclone_service 'icloud' 'iCloudDrive'
 
 printf 'RClone Systemd Integration Enabled!!\n'
 
